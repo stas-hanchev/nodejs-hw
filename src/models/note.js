638888453b116d473b0a6b8d055b248b1c693273
Stanislav Hanchev <stanislav@smoothr.de>
2025-12-21 17:43:18 +0200

    finish

---
+++ src/models/note.js
@@ -0,0 +1,30 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+const noteSchema = new Schema(
+ {
+  title: {
+   type: String,
+   required: true,
+   trim: true,
+  },
+  content: {
+   type: String,
+   required: false,
+   trim: true,
+   default: '',
+  },
+  tag: {
+   type: String,
+   required: false,
+   enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+   default: 'Todo',
+  },
+ },
+ {
+  timestamps: true
+ },
+);
+
+export const Note = model('Student', noteSchema);
+
--- src/server.js
+++ src/server.js
@@ -1,58 +1,33 @@
 import express from 'express';
 import 'dotenv/config';
+
 import cors from 'cors';
-import pino from 'pino-http';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { logger } from './middleware/logger.js';
+
+import notesRouter from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
-
 app.use(cors());
-
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello world!' });
 });
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
+app.use(notesRouter);
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: `${err.message}`
-  });
-});
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
