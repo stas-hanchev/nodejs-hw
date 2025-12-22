8a182a1fcdfa0cf4c640fb4d93b7c885b254d792
Stanislav Hanchev <stanislav@smoothr.de>
2025-12-22 17:09:12 +0200

    fix

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -14,7 +14,7 @@ import {
   noteIdSchema,
   createNoteSchema,
   updateNoteSchema,
-} from '../validations/notesValidation.js';
+} from '../validation/notesValidation.js';
 
 const router = Router();
 
