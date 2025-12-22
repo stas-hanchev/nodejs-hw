527250cd583db34fafd9db4913acdff421515329
Stanislav Hanchev <stanislav@smoothr.de>
2025-12-22 17:00:27 +0200

    finish (add pagination and filters)

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,9 +2,33 @@ import createHttpError from 'http-errors';
 import { Note } from "../models/note.js";
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  console.log('getAllNotes', notes);
-  res.status(200).json(notes);
+  const { page = 1, perPage = 10, tag, search } = req.query;
+  const skip = (page - 1) * perPage;
+
+  const notesQuery = Note.find();
+
+  if (search) {
+    notesQuery.where({ $text: { $search: search } });
+  }
+
+  if (tag) {
+    notesQuery.where("tag").equals(tag);
+  }
+
+  const [totalItems, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalItems,
+    totalPages,
+    notes,
+  });
 };
 
 export const getNoteById = async (req, res) => {
--- src/models/note.js
+++ src/models/note.js
@@ -1,30 +1,17 @@
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
+import { TAGS } from '../constants/tags';
 
 const noteSchema = new Schema(
  {
-  title: {
-   type: String,
-   required: true,
-   trim: true,
-  },
-  content: {
-   type: String,
-   required: false,
-   trim: true,
-   default: '',
-  },
-  tag: {
-   type: String,
-   required: false,
-   enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
-   default: 'Todo',
-  },
+  title: { type: String, required: true, trim: true, },
+  content: { type: String, required: false, trim: true, default: '', },
+  tag: { type: String, required: false, enum: TAGS, default: 'Todo', },
  },
- {
-  timestamps: true
- },
+ { timestamps: true },
 );
+
+noteSchema.index({ title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
 
